3f5f5840e872b1e1139ad5f094d5099043721f27
DESKTOP-SJ1IHQ3\Uriel <l20140185@queretaro.tecnm.mx>
2026-03-15 22:45:22 -0600

    asd

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,8 +1,13 @@
 from .utils import get_habita_user, is_habita_authenticated
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def habita_auth(request):
+    habita_user = get_habita_user(request)
+    logger.warning(f"habita_auth context processor called. User: {habita_user}, Authenticated: {is_habita_authenticated(request)}")
     return {
         "habita_logged_in": is_habita_authenticated(request),
-        "habita_user": get_habita_user(request),
+        "habita_user": habita_user,
     }
